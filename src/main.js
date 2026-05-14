5f4758168af8be1dfd52842709621d93a6bf6ff4
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-05-14 15:21:13 -0300

    HTML do display feito

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
