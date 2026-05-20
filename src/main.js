b290b6822d0ffa92f085db56a129ac6762ac7979
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-05-20 00:54:49 -0300

    concatenagem dos números no display feitos, mudança nos ids por data-*

--- src/main.js
+++ src/main.js
@@ -16,3 +16,15 @@ function divide(a, b) {
     return a / b;
 }
 
+let valorAtual = "";
+let valorAnterior;
+let operador = document.querySelectorAll(".operators-btn");
+let numbers = document.querySelectorAll(".number-btn");
+
+
+numbers.forEach(button => {
+    button.addEventListener("click", () => {
+        valorAtual =  valorAtual + button.dataset.value;
+        document.getElementById("current-display").innerText = valorAtual;
+    });
+});
